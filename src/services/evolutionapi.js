8b3068a568d1f92f503d9e1bac5bdfc2c45cdaa2
RafaelApfernandes97 <109108455+RafaelApfernandes97@users.noreply.github.com>
2025-08-08 10:44:08 -0300

    Number

--- src/services/evolutionapi.js
+++ src/services/evolutionapi.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 
 const EVOLUTION_API_URL = 'https://whatsapp.oballetemfoco.com/';
-const INSTANCE = 'Balletemfoco_2';
+const INSTANCE = 'balletemfoco_bkp';
 const APIKEY = '4LZrB0AxrjYDyr5GwxKSOCIwIf8LI2rp';
 
 async function sendOrderSummary({ numero, mensagem }) {
@@ -52,5 +52,6 @@ async function sendOrderSummary({ numero, mensagem }) {
     throw error; // Re-throw para ser capturado na rota
   }
 }
+
 
 module.exports = { sendOrderSummary }; 
